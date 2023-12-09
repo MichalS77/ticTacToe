15d1ece4bb837fbfa8a08308496c376a12e75656
Michal S <minitrikus@gmail.com>
2023-12-09 17:39:28 +0100

    game controller

--- tic.js
+++ tic.js
@@ -9,37 +9,89 @@ function GameBoard(){
             board[i].push(Cell());
         }
     }
+
     const getBoard = () => board;
+    
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) =>
+        row.map((cell) => cell.getValue()))
+        console.log(boardWithCellValues);
+    };
+    
+    const dropToken = (row, column, activePlayer) => {
+       board[row][column].addToken(activePlayer);
+    }
+
+return {getBoard, printBoard, dropToken};
+}
 
 function Cell(){
     let value = 0;
 
-    const addToken = (player) => value = player;
+    const addToken = (player) => value = player.token;
 
     const getValue = () => value;
 
-    return  {addToken, getValue};
+return  {addToken, getValue};
 }
 
-const printBoard = () => {
-    const boardWithCellValues = board.map((row) =>
-    row.map((cell) => cell.getValue()))
-    console.log(boardWithCellValues);
-};
-return {getBoard, printBoard};
+function player(){
+
+    function createPlayer(name, token){
+
+return {name, token};
 }
 
-GameBoard().printBoard();
-
-function changePlayers(){
-    if(activePlayer == player1){
-        activePlayer == player2;
-    } else activePlayer == player1;
+return {createPlayer};
 }
 
-function createPlayer(name, token){
-    return {name, token};
+function gameController(){
+    let player1 = player().createPlayer('player1', 'X');
+    let player2 = player().createPlayer('player2', 'O');
+    let activePlayer = player1;
+
+    const changePlayers = (activePlayer) => {
+        if(activePlayer.name == player1.name){
+            return player2;
+        } else {
+            return player1;
+        }
+    }
+
+    
+    const getActivePlayer = () => activePlayer;
+    
+    let gameBoard = GameBoard();
+    let board = gameBoard.getBoard();
+    const printNewRound = () => {
+        
+        gameBoard.printBoard();
+        
+        console.log(`${activePlayer.name}'s turn!`);
+    }
+    
+    printNewRound();
+    
+    let playRound = (row,column) => {
+        console.log(`Dropping ${activePlayer.name}'s token into row ${row} and column ${column}`);
+
+        gameBoard.dropToken(row, column, activePlayer)
+        
+        activePlayer = changePlayers(activePlayer);
+        
+        printNewRound();
+        }
+        return {playRound, getActivePlayer, changePlayers};
 }
+
+let game = gameController();
+
+
+
+
+
+
+
 
 
 
