8e67b504c77d15be78159c7f6f78a17c4f5d6eb6
Michal S <minitrikus@gmail.com>
2023-12-11 21:18:17 +0100

    checkWinner

--- tic.js
+++ tic.js
@@ -58,11 +58,13 @@ function gameController(){
         }
     }
 
+    const three = (arr) => arr.every(Cell => Cell.getValue() === 'X' || Cell.getValue() == 'O');
     
     const getActivePlayer = () => activePlayer;
     
     let gameBoard = GameBoard();
     let board = gameBoard.getBoard();
+
     const printNewRound = () => {
         
         gameBoard.printBoard();
@@ -71,27 +73,41 @@ function gameController(){
     }
     
     printNewRound();
+    let checkWinner = () =>{
+        let board = gameBoard.getBoard();
+        const flatBoard = board.flatMap(row => row);
+        
+        let col1 = [flatBoard[0], flatBoard[3], flatBoard[6]];
+        let col2 = [flatBoard[1], flatBoard[4], flatBoard[7]];
+        let col3 = [flatBoard[2], flatBoard[5], flatBoard[8]];
+        let diag1 = [flatBoard[0], flatBoard[4], flatBoard[8]];
+        let diag2 = [flatBoard[2], flatBoard[4], flatBoard[6]];
+
+        if(three(board[0]) || three(board[1]) || three(board[2]) || three(col1) || three(col2) || three(col3) || three(diag1) || three(diag2))
+        return console.log(`${activePlayer.name} won`);
+        }
     
     let playRound = (row,column) => {
         console.log(`Dropping ${activePlayer.name}'s token into row ${row} and column ${column}`);
 
-        gameBoard.dropToken(row, column, activePlayer)
+        gameBoard.dropToken(row, column, activePlayer);
+        
+        checkWinner();
         
         activePlayer = changePlayers(activePlayer);
         
         printNewRound();
-        }
-        return {playRound, getActivePlayer, changePlayers};
+    }
+
+        return {playRound, getActivePlayer, changePlayers, board, checkWinner};
+
 }
 
+
+
+    
+
 let game = gameController();
-
-
-
-
-
-
-
 
 
 
